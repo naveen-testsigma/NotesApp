f30d380844d44dbb6747586d3ca4a670081c52a9
naveentestsigma <naveenkumar.chandrasekar@testsigma.com>
2023-03-28 15:34:16 +0530

    h

--- NotesUI/src/app/app-routing.module.ts
+++ NotesUI/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./WebModule/login/login.component";
+import {SignupComponent} from "./WebModule/signup/signup.component";
+import {DashboardComponent} from "./WebModule/dashboard/dashboard.component";
+import {HomeComponent} from "./WebModule/home/home.component";
 
 const routes: Routes = [
-  {}
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'',component:HomeComponent},
+
 ];
 
 @NgModule({
--- NotesUI/src/app/app.module.ts
+++ NotesUI/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './WebModule/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './WebModule/login/login.component';
+import { SignupComponent } from './WebModule/signup/signup.component';
+import { DashboardComponent } from './WebModule/dashboard/dashboard.component';
+import { HomeComponent } from './WebModule/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
