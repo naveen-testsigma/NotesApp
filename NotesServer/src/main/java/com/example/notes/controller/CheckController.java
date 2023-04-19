d903645a19c8f9ffaf8a745a4f833c0b1a5727bc
Moneshwar <moneshwar.c@testsigma.com>
2023-04-19 13:08:59 +0530

    mmm

--- NotesServer/src/main/java/com/example/notes/controller/CheckController.java
+++ NotesServer/src/main/java/com/example/notes/controller/CheckController.java
@@ -1,8 +1,9 @@
 package com.example.notes.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin("http://localhost:4200")
 @RestController
 public class CheckController {
     @GetMapping("/")
